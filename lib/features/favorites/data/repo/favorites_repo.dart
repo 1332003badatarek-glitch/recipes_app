75f21fd4fa29cd8caba741af55cfb0266d5a66bd
abdallah tarek <1332003badatarek@gmail.com>
2025-12-24 22:57:42 +0200

    edit faforites repo to using dart z package

---
+++ lib/features/favorites/data/repo/favorites_repo.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:recipesapp/core/models/models/recipes_model.dart';
+import 'package:recipesapp/features/favorites/data/data_source/favorites_local_data_source.dart';
+import 'package:recipesapp/features/favorites/errors/favorites_failure.dart';
+
+class FavoritesRepo {
+  final FavoritesLocalDataSource local;
+
+  FavoritesRepo({required this.local});
+
+  Future<Either<FavoritesFailure, List<RecipesModel>>> getFavorites() async {
+    try {
+      final favorites = local.getFavorites();
+      return Right(favorites);
+    } catch (_) {
+      return const Left(
+        FavoritesFailure("Failed to load favorites"),
+      );
+    }
+  }
+
+  Future<Either<FavoritesFailure, void>> toggleFavorite(
+    RecipesModel recipe,
+  ) async {
+    try {
+      await local.toggleFavorite(recipe);
+      return const Right(null);
+    } catch (_) {
+      return const Left(
+        FavoritesFailure("Failed to update favorites"),
+      );
+    }
+  }
+
+  bool isFavorite(int recipeId) {
+    try {
+      return local.isFavorite(recipeId);
+    } catch (_) {
+      return false;
+    }
+  }
+}
