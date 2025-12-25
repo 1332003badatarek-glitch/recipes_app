209ab2f6e2fc81014e0513a3bbd5e7195a254843
abdallah tarek <1332003badatarek@gmail.com>
2025-12-25 18:47:34 +0200

    adding the functions of the cubit 1- load favorites function to get the favorites from local storage 2- is favorite function for checking if this item saved as favorite before 3- toggle favorite to save and remove any item this based on this item saved as favorite before or not

--- lib/features/favorites/cubits/favorites_cubit/favorites_cubit.dart
+++ lib/features/favorites/cubits/favorites_cubit/favorites_cubit.dart
@@ -1,8 +1,53 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recipesapp/core/models/models/recipes_model.dart';
+import 'package:recipesapp/features/favorites/data/repo/favorites_repo.dart';
+import 'package:recipesapp/features/favorites/errors/favorites_failure.dart';
 
 part 'favorites_state.dart';
 
 class FavoritesCubit extends Cubit<FavoritesState> {
-  FavoritesCubit() : super(FavoritesInitial());
+  final FavoritesRepo repo;
+  FavoritesCubit(this.repo) : super(FavoritesInitial());
+
+  final Set<int> _favoriteIds = {};
+
+  void loadFavorites() async {
+    emit(FavoritesLoading());
+    final result = await repo.getFavorites();
+    result.fold(
+      (error) {
+        emit(FavoritesFailure(error: error));
+      },
+      (favorites) {
+        final favoritesList = favorites;
+        _favoriteIds
+          ..clear()
+          ..addAll(favorites.map((e) => e.id));
+        emit(FavoritesLoaded(favoritesList: favoritesList));
+      },
+    );
+  }
+
+  bool isFavorite(int recipeId) {
+    return _favoriteIds.contains(recipeId);
+  }
+
+  Future<void> toggleFavorite(RecipesModel recipe) async {
+    final result = await repo.toggleFavorite(recipe);
+    result.fold(
+      (error) {
+        emit(FavoritesFailure(error: error));
+      },
+      (_) {
+        if (_favoriteIds.contains(recipe.id)) {
+          _favoriteIds.remove(recipe.id);
+        } else {
+          _favoriteIds.add(recipe.id);
+        }
+        emit(FavoritesUpdated());
+      },
+    );
+  }
 }
