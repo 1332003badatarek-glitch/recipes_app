e2001b7ec1d8f70ad26363e1729f03f45d1cff36
abdallah tarek <1332003badatarek@gmail.com>
2025-12-25 18:44:39 +0200

    add the states of the favorites cubit

--- lib/features/favorites/cubits/favorites_cubit/favorites_state.dart
+++ lib/features/favorites/cubits/favorites_cubit/favorites_state.dart
@@ -5,3 +5,18 @@ abstract class FavoritesState {}
 
 class FavoritesInitial extends FavoritesState {}
 
+class FavoritesLoading extends FavoritesState {}
+
+class FavoritesLoaded extends FavoritesState {
+  final List<RecipesModel> favoritesList;
+
+  FavoritesLoaded({required this.favoritesList});
+}
+
+class FavoritesFailure extends FavoritesState {
+  final FavoritesException error;
+
+  FavoritesFailure({required this.error});
+}
+
+class FavoritesUpdated extends FavoritesState {}
