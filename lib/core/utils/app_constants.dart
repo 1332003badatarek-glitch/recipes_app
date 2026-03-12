672135d7ffc65732f10f42f33e40cccdbbc24a98
abdallah tarek <1332003badatarek@gmail.com>
2026-03-12 21:48:40 +0200

    create app constants for asset image names

---
+++ lib/core/utils/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  AppConstants._();
+
+  static String appetizer = 'assets/images/appetizer.jpg';
+  static String breakfast = 'assets/images/breakfast.png';
+  static String dessert = 'assets/images/dessert.png';
+  static String dinner = 'assets/images/dinner.png';
+  static String snack = 'assets/images/snack.jpg';
+  static String lunch = 'assets/images/lunch.jpg';
+}
